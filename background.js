d094a4ef799ce7cf794644a772fb14383c64749e
thuanGIT <thuan.vo@github.com>
2020-10-31 16:05:34 -0700

    update

--- background.js
+++ background.js
@@ -1,18 +1,11 @@
-chrome.runtime.onInstalled.addListener(function() {
+const defaultFilters = [
+	"*://*.facebook.com/*",
+    "*://*.youtube.com/*",
+    "*://*.instagram.com/*"
+]
 
-    //Event listener
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-
-    //Add declared rules 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-                })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+);
