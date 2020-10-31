fe61e79793a5e7b0b8b6823b31e963c18d2d70f9
Jill Phan <jill01009@gmail.com>
2020-10-31 15:41:23 -0700

    Site-blocking

---
+++ background.js
@@ -0,0 +1,11 @@
+const defaultFilters = [
+	"*://*.facebook.com/*",
+    "*://*.youtube.com/*",
+    "*://*.instagram.com/*"
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
