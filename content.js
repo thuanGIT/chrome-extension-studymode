125fdb293b7b1cab909b61edaba1d4e2247cfa4d
thuanGIT <thuan.vo@github.com>
2020-10-31 20:52:05 -0700

    update

--- content.js
+++ content.js
@@ -1,4 +1,8 @@
-var div=document.createElement('div'); 
-document.body.appendChild(div); 
-div.innerText='test123';
-div.style.backgroundColor='red';
+//var div=document.createElement('div'); 
+//document.body.appendChild(div); 
+
+var myImage = new Image(100, 200);
+myImage.src = 'jumpscare.jpg';
+
+div.style.backgroundColor='black';
+document.body.appendChild(myImage);
